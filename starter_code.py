f08e2940bf21bea30f5eb957d498e9acd609e223
Auman <info@auman.me>
2018-09-19 04:53:02 -0400

    Add round score keeper

--- starter_code.py
+++ starter_code.py
@@ -27,6 +27,8 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.p1score = 0
+        self.p2score = 0
 
     def play_round(self):
         move1 = self.p1.move()
@@ -39,8 +41,10 @@ class Game:
         if move1 != move2:
             if beats(move1, move2):
                 print("Player 1 is the winner")
+                self.p1score += 1
             else:
                 print("Player 2 is the winner")
+                self.p2score += 1
         else:
             print("It's a Tie")
 #
@@ -52,7 +56,8 @@ class Game:
             print(f"Round {round}:")
             self.play_round()
         print("Game over!")
-
+        print(f"Player 1 score: {self.p1score}")
+        print(f"Player 2 score: {self.p2score}")
 
 class RandomPlayer(Player):
     def move(self):
